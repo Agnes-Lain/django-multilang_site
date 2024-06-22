aba319b6bb4f24e2c89cb799210c72f3016fd4f3
yizhu zong <agnes.lain@gmail.com>
2024-06-22 16:31:24 +0200

    add multi-lang feature and rosetta package to manage translation

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+# import tool to manage translation
+from django.utils.translation import gettext_lazy as _
 
 # Create model Article.
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    publication_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(_("title"), max_length=255)
+    content = models.TextField(_("content"))
+    publication_date = models.DateTimeField(_("publication_date"),auto_now_add=True)
     # use the title to dispay in admin for each created instance.
     def __str__(self):
         return self.title
--- multilang_site/urls.py
+++ multilang_site/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls'))
-]
+urlpatterns = i18n_patterns(
+    path(_('admin/'), admin.site.urls),
+    path('rosetta/', include('rosetta.urls')), #use rosetta translation interface
+    path('', include('main.urls')),
+)
