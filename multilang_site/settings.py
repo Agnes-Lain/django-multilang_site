64c8f4bd12b9fc8f8dd2f179b48c4bbb52c42ac6
yizhu zong <agnes.lain@gmail.com>
2024-06-24 16:10:27 +0200

    add .env file to manage local credentials

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
 # Database setup for render.com deployment to overide
 database_url = os.environ.get("DATABASE_URL")
 
-DATABASES["default"] = dj_database_url.parse(database_url)
+# DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 
@@ -169,15 +169,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 STATIC_URL = 'static/'
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
-# if not DEBUG:
-#     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
