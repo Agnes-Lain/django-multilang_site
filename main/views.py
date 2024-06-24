db57edfa2e1e26fc398148e4e9a4fb621cb0cc8c
yizhu zong <agnes.lain@gmail.com>
2024-06-24 22:20:10 +0200

    add comments in view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
 from django.utils.translation import get_language
 # import the model
 from main.models import Article
@@ -15,12 +14,13 @@ def article_index(request):
     }
     return render (request, "article/index.html", context)
 
+# Create a view for one article detail with giving id. Inside, nested the routing for delete the showing article
 def article_show(request, pk):
     article = Article.objects.get(pk=pk)
     lang = get_language()
     if request.method=="POST":
         article.delete()
-        return redirect('/%s' % lang)
+        return redirect('/%s' % lang) # redirect to the corresponding language root
 
 
     context = {
@@ -28,6 +28,7 @@ def article_show(request, pk):
     }
     return render(request, "article/show.html", context)
 
+# Create a form for adding new article to database
 def article_new(request):
     # initiate an empty dict to stock the data
     context = {}
@@ -39,7 +40,7 @@ def article_new(request):
 
         if form.is_valid():
             form.save()
-        return redirect('/%s' % lang)
+        return redirect('/%s' % lang) # redirect to the corresponding language root
 
     context['form'] = form
     return render(request, 'article/new.html', context)
